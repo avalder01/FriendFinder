8d62be237443ebc2a01440262b81ecf142c425f0
Allie Valder <avalder19@gmail.com>
2019-01-06 00:23:22 -0500

    project

---
+++ app/data/friends.js
@@ -0,0 +1,31 @@
+// This holds the friends information
+var friends = [
+    { 
+    "name": "Harry Potter",
+    "photo": "https://images.ctfassets.net/bxd3o8b291gf/3SQ3X2km8wkQIsQWa02yOY/25f258f21bdbe5f552a4419bb775f4f0/HarryPotter_WB_F4_HarryPotterMidshot_Promo_080615_Port.jpg?w=1200", 
+    "scores": ["3", "3", "3", "1", "5", "2", "5", "3", "1", "4"]
+}, {
+    "name": "Hermione Granger", 
+    "photo": "https://imagesvc.timeincapp.com/v3/mm/image?url=https%3A%2F%2Fimages.hellogiggles.com%2Fuploads%2F2016%2F12%2F21054805%2Femma_watson_hermione_granger_4k-HD.jpg&w=1000&c=sc&poi=face&q=70", 
+    "scores": ["4", "3", "4", "3", "2", "2", "3", "2", "4", "5"] 
+}, { 
+    "name": "Ron Weasley", 
+    "photo": "https://fsmedia.imgix.net/b3/c0/e1/b5/bb7d/47fe/80bc/5eafe2598c96/ron-weasley-sad-person.jpeg?rect=0%2C54%2C1024%2C512&dpr=2&auto=format%2Ccompress&w=650", 
+    "scores": ["5", "1", "5", "4", "5", "1", "2", "5", "4", "1"]  
+}, { 
+    "name": "Lavender Brown",
+    "photo": "https://vignette.wikia.nocookie.net/harry-potter-pedia/images/5/57/LavenderBrown.jpg/revision/latest?cb=20140511064822", 
+    "scores": ["1", "4", "4", "2", "2", "1", "3", "2", "2", "3"] 
+}, {
+    "name": "Ginny Weasley",
+    "photo": "http://images4.fanpop.com/image/photos/22600000/Deathly-Hallows-Part-1-promo-bonnie-wright-22635226-404-540.jpg",
+    "scores":["3", "4", "5", "2", "1", "1", "3", "2", "5", "5"]  
+}, { 
+    "name": "Dean Thomas",
+    "photo": "https://vignette.wikia.nocookie.net/harrypotterfanon/images/a/a5/Dean_Thomas.jpg/revision/latest?cb=20160412000218",
+    "scores":  ["1", "2", "5", "1", "3", "2", "2", "1", "3", "2"]
+}
+];
+
+
+module.exports = friends;
